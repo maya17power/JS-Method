46a4f341147e5b5dbc39cb57269c66a5094c629c
maya17power <tavo1.ortega1@gmail.com>
2017-09-02 14:15:48 -0700

    Adding forEach() method to repo.

---
+++ forEach/README.md
@@ -0,0 +1,1 @@
+.forEach() - Method // executes a provided funtion once for each array element
---
+++ forEach/forEach.js
@@ -0,0 +1,11 @@
+/*
+.forEach() - Method // executes a provided funtion once for each array element
+*/
+
+var myArray = [1,2,3,4,5,6,7];
+
+console.log(myArray);
+
+//multiply each element in array by 2.
+
+myArray.forEach(function(val){console.log(val * 2);});
