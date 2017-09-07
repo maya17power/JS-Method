a5caf51af551b84dd5781c31e8ea13a8983f1a61
maya17power <tavo1.ortega1@gmail.com>
2017-09-06 22:05:08 -0700

    Adding split method to repo.

---
+++ split/README.md
@@ -0,0 +1,2 @@
+//split strings using split();
+//split string and push it as an array.
---
+++ split/split.js
@@ -0,0 +1,11 @@
+//split strings using split();
+//split string and push it as an array.
+
+var string = "Split me into an array";
+var array = [];
+
+// Only change code below this line.
+
+array = string.split([" "]);
+console.log(array.length);
+console.log(array);
