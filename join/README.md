c764ed7b503c5fbe0dd6dd5a6eae47eaa034e012
maya17power <tavo1.ortega1@gmail.com>
2017-09-06 21:54:45 -0700

    Adding join method to repo.

---
+++ join/README.md
@@ -0,0 +1,3 @@
+.join(' ');
+//join arrays together using a delimiter and convert to string.
+// get array and join as a string with spaces.
---
+++ join/join.js
@@ -0,0 +1,9 @@
+//join arrays together using a delimiter and convert to string.
+// get array and join as a string with spaces.
+
+var joinMe = ["Split","me","into","an","array"];
+var joinedString = '';
+
+// Only change code below this line.
+
+joinedString = joinMe.join(" ");
